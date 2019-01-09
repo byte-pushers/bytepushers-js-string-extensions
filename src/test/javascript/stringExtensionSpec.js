84095e2f69e11d10c428ddf6671807cd897ed7a6
Tonté Pouncil <pouncilt@Tontes-MacBook-Pro-2.local>
2019-01-09 16:10:52 -0600

    Added 5 Jasmine tests for String.Trim function.

--- src/test/javascript/stringExtensionSpec.js
+++ src/test/javascript/stringExtensionSpec.js
@@ -1,10 +1,10 @@
-define(['bytepushers'], function(BytePushers) {
+define(['bytepushers'], function (BytePushers) {
     describe("String extension tests:", function () {
 
         var testStr = "Hello this is a random string";
 
-        describe('String.prototype.trim', function() {
-            it('will trim whitespace correctly', function() {
+        describe('String.prototype.trim', function () {
+            it('will trim whitespace correctly', function () {
                 var result = "                       l     shdasdk;lg;al j          ".trim();
 
                 expect(result).toBe("l     shdasdk;lg;al j");
@@ -34,38 +34,71 @@ define(['bytepushers'], function(BytePushers) {
                 expect(actualResult).toBe(expectedResult);
 
             });
+
+            it('trim white space before and after string but not space between letters', function () {
+                var testString = " a b c ";
+                var expectedResult = "a b c";
+                var actualResult = testString.trim();
+
+                expect(actualResult).toBe(expectedResult);
+            });
+
+            it('trim white space before and after string but not space between words', function () {
+                var testString = " Donkey Kong ";
+                var expectedResult = "Donkey Kong";
+                var actualResult = testString.trim();
+
+                expect(actualResult).toBe(expectedResult);
+            });
+
+            it('trim white space after string but not space between words', function () {
+                var testString = "Duck Hunt ";
+                var expectedResult = "Duck Hunt";
+                var actualResult = testString.trim();
+
+                expect(actualResult).toBe(expectedResult);
+            });
+
+            it('white space will not be trimmed if there is no white space', function () {
+                var testString = "Ness";
+                var expectedResult = "Ness";
+                var actualResult = testString.trim();
+
+                expect(actualResult).toBe(expectedResult);
+            });
+
         });
 
-        describe('String.prototype.includes', function() {
-            it('can tell if a string includes a string', function() {
+        describe('String.prototype.includes', function () {
+            it('can tell if a string includes a string', function () {
                 var result = testStr.includes("o this i");
 
                 expect(result).toBe(true);
             });
 
-            it('can tell if a string does not include a string', function() {
+            it('can tell if a string does not include a string', function () {
                 var result = testStr.includes("This is not in the string");
 
                 expect(result).toBe(false);
             });
         });
 
-        describe('String.prototype.toCamelCase', function() {
-            it('can format a sentence into camel case', function() {
+        describe('String.prototype.toCamelCase', function () {
+            it('can format a sentence into camel case', function () {
                 var result = "Hello this is a sentence.".toCamelCase();
 
                 expect(result).toBe("helloThisIsASentence.");
             });
         });
 
-        describe('String.prototype.toNormalCase', function() {
-            it('can format a sentence into camel case', function() {
+        describe('String.prototype.toNormalCase', function () {
+            it('can format a sentence into camel case', function () {
                 var result = "Hello world".toCamelCase().toNormalCase();
                 // helloWorld => "Hello World"
                 expect(result).toBe("Hello World");
             });
 
-            it('can format a sentence into camel case with a single letter word', function() {
+            it('can format a sentence into camel case with a single letter word', function () {
                 var result = "Hello this is a sentence".toCamelCase().toNormalCase();
                 // Would think it would do this
                 // Hello this is a sentence => helloThisIsASentence => Hello This Is A Sentence
@@ -75,11 +108,11 @@ define(['bytepushers'], function(BytePushers) {
             });
         });
 
-        describe('String.format', function() {
-            it('can format a string properly', function() {
+        describe('String.format', function () {
+            it('can format a string properly', function () {
                 var result = String.format("Hello {1} is {2} random {3}", "this", "a", "string");
 
-                expect(result).toBe(testStr);  testStr = "Hello this is a random string";
+                expect(result).toBe(testStr);  //testStr = "Hello this is a random string";
             });
 
             it('can format a string properly with punctuation', function() {
